5967902c43b5e76ce863cd0f76d6c14f8a055a99
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2024-01-10 02:02:30 +0530

    Update productModels.js

--- model/productModels.js
+++ model/productModels.js
@@ -4,6 +4,18 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   // Implement the product schema fields here as
   // productName as String and price as Number
+   productName: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
 });
 
+
 // Create and export the Product model
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
